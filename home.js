1382d922c31f1013b248b36f7040c8f87eac73f3
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-10 23:02:47 +0300

    .112

--- home.js
+++ home.js
@@ -26,6 +26,6 @@ const Animal4 = new Animal('Human', 75, 'Male', 'Mammal');
 
 
 console.log(Animal1.info());
-console.log(.info());
+console.log(Animal2.info());
 console.log(.info());
 console.log(.info());
