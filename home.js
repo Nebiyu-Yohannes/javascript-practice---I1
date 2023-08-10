55978cf8e137eebf8f66e61ec44919f2f423c343
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-10 22:35:00 +0300

    .12

--- home.js
+++ home.js
@@ -1,2 +1,13 @@
 // objects, classes, instances and constructors
 
+class Animal{
+    
+    constructor(name, age, sex, type){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.Type = type;
+    }
+
+
+}
