259e6c9d73e3ffc6439a12a95b59e5c7612bd158
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-10 22:59:17 +0300

    INSTANCES

--- home.js
+++ home.js
@@ -15,9 +15,16 @@ class Animal{
 
 }
 
-const Dog = new Animal('Dog', 7, 'male', 'mammal');
+const Animal1 = new Animal('Dog', 7, 'male', 'Mammal');
+
+const Animal2 = new Animal('Cat', 4, 'female', 'Mammal');
+
+const Animal3 = new Animal('Eagle', 3, 'male', 'Bird');
+
+const Animal4 = new Animal('Human', 75, 'male', 'Mammal');
 
 console.log(Dog);
 console.log(Dog.Type);
 console.log(Dog.info());
+
 
