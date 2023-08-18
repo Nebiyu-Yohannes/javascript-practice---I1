ea42698ebc806b14771006cc93a96dceb51fd6b7
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-18 03:14:36 +0300

    edit

--- class_inheritance.js
+++ class_inheritance.js
@@ -1,24 +1,42 @@
 
 // Inheritance - parent and child /class/
 
-
 class HospitalEmployee {
-  constructor(name){
-    this._name = name;
-    this._remainingVacationDays = 20;
-  }
+    constructor(name) {
+        this._name = name;
+        this._remainingVacationDays = 20;
+    }
 
-  get name(){
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  get remainingVacationDays(){
-    return this._remainingVacationDays;
-  }
+    get remainingVacationDays() {
+        return this._remainingVacationDays;
+    }
 
-  takeVacationDays(daysOff){
-    this._remainingVacationDays = this._remainingVacationDays - daysOff; 
-  }
+    takeVacationDays(daysOff) {
+        this._remainingVacationDays -= daysOff;
+    }
+}
 
-  }
+
+class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+        super(name);
+        this._certifications = certifications;
+        this._remainingVacationDays = 20;
+    }
+
+    get remainingVacationDays() {
+        return this._remainingVacationDays;
+    }
+
+    get certifications() {
+        return this._certifications;
+    }
+}
+
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
 
